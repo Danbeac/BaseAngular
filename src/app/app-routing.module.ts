df8249520b3cab56e3d0e2c19b997e3a207908bd
d4nbeac <danbeac@hotmail.com>
2022-04-16 01:55:16 -0500

    Pages Section added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,21 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    children: [
+        {
+            path: 'login',
+            loadChildren: () => import('./pages/login/login.module').then(m => m.LoginModule)
+        }
+    ]
+}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+
