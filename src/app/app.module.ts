e5332cf71385bb5e745d2cf23a8bc0dfe2ccb723
d4nbeac <danbeac@hotmail.com>
2022-04-17 17:12:31 -0500

    Datepicker component is added in Shared controls-section

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,6 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+// Date
+import { MatNativeDateModule, MatDateFormats, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
+
+const APP_DATE_FORMATS: MatDateFormats = {
+    parse: {
+        dateInput: { day: 'numeric', month: 'numeric', year: 'numeric' },
+    },
+    display: {
+        dateInput: { day: 'numeric', month: 'short', year: 'numeric' },
+        monthYearLabel: { year: 'numeric', month: 'short' },
+        dateA11yLabel: { year: 'numeric', month: 'long', day: 'numeric' },
+        monthYearA11yLabel: { year: 'numeric', month: 'long' }
+    }
+};
+
 @NgModule({
   declarations: [
     AppComponent
@@ -12,9 +27,13 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MatNativeDateModule
   ],
-  providers: [],
+  providers: [
+      { provide: MAT_DATE_LOCALE, useValue: 'en-GB' },
+      { provide: MAT_DATE_FORMATS, useValue: APP_DATE_FORMATS }
+  ],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
